50dad0ceea3dff14ebc62404e5a433b1179fb8e4
dt009715 <80246805+dt009715@users.noreply.github.com>
2024-03-25 11:53:25 +0100

    test

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-const bla = 2;
-console.log(bla);
+const tasks = new map();
 const status = false;
-const list = [];
 const button = document.querySelector('button');
 const task = document.getElementById('message');
 const getList = document.getElementById('list');
 const button1 = document.getElementById('button1');
 
-const onClick = () => button.addEventListener('click', (event) => {
-    list.push(task.value)
-})
+
+const add = button1.addEventListener('click', (event) => { list.push(task.value)})
+
  const TodoList = () => {
     return {
-        addTo(task){
-            if (button1.onClick()) {
-                list.push(task.value);
-                
-            }    
-        },
-        updateList(){
-            return li.value = list;
-            
+        if(add){
+            let newTask = document.createElement('li');
+            li.value = task.value;
         }
-        
     }
  }
 
@@ -31,4 +22,3 @@ const onClick = () => button.addEventListener('click', (event) => {
 
  }
  TodoList();
-    console.log(list)
